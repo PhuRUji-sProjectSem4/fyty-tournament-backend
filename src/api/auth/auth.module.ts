062db0248f42e43d6d59e7cdd521106650d5b4fe
PhuRuji <59276795+phubet16132@users.noreply.github.com>
2022-11-10 17:06:13 +0700

    api file layout not finish

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-
 import { PrismaService } from 'src/prisma.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
